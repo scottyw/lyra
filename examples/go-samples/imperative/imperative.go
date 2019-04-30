b5dff8354af76341b9056a15a88d0c0f46a4ad77
Scott Walker <scott.walker@puppet.com>
2019-04-30 15:56:37 +0100

    nebula

--- examples/go-samples/imperative/imperative.go
+++ examples/go-samples/imperative/imperative.go
@@ -1,62 +1,56 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
-	"strings"
-
-	"github.com/hashicorp/go-hclog"
 	"github.com/lyraproj/servicesdk/lang/go/lyra"
+	"os/exec"
 )
 
-func main() {
-	type out struct {
-		F string
+func runContainer(image string, in map[string]string) (map[string]string, error) {
+	// Marshal the args into JSON
+	input, err := json.Marshal(&in)
+	if err != nil {
+		return nil, err
 	}
+
+	inputS := fmt.Sprintf("INPUT=%s ", input)
+	cmd := exec.Command("docker", "run", "-t", "--env", inputS, image)
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal JSON output from the run
+	out := map[string]string{}
+	err = json.Unmarshal(output, &out)
+	return out, err
+}
+
+type containerResult struct {
+	Result string
+}
+
+func main() {
 
 	// Workflow input is from Hiera and a constant (declared by annotations in the In struct).
 	lyra.Serve(`imperative_go`, nil, &lyra.Workflow{
 		Parameters: struct {
-			A string `lookup:"golang.hello"`
-			B int    `value:"5"`
+			Image string `lookup:"nebula.image"`
 		}{},
-		Return: out{},
+		Return: containerResult{},
 		Steps: map[string]lyra.Step{
 
-			// The Actions here uses anonymous structs to describe input and output. They can of
-			// course also use named structs.
-
-			// First and second will execute in parallel
-			`first`: &lyra.Collect{
-				// The step will be executed five times in parallel and the result will be collected
-				// into an output variable named 'C' which is a slice with five elements of the output from
-				// the producer.
-				Times:  5,
-				As:     struct{ Idx int }{},
-				Return: `C`,
-
-				Step: &lyra.Action{
-					Do: func(input struct {
-						A   string
-						Idx int
-					}) struct{ C string } {
-						hclog.Default().Info("first", "A", input.A, "Idx", input.Idx)
-						return struct{ C string }{input.A + ` #` + strconv.Itoa(input.Idx)}
-					}}},
-
-			`second`: &lyra.Action{
-				Do: func(input struct{ B int }) struct{ D int } {
-					hclog.Default().Info("second", "B", input.B)
-					return struct{ D int }{input.B}
-				}},
-
-			// Third takes input from first and second
-			`third`: &lyra.Action{
+			`run`: &lyra.Action{
 				Do: func(input struct {
-					C []string
-					D int
-				}) out {
-					hclog.Default().Info("third", "C", input.C, "D", input.D)
-					return out{fmt.Sprintf("%s - %d", strings.Join(input.C, `, `), input.D)}
+					Image string
+				}) containerResult {
+					out, err := runContainer(input.Image, map[string]string{"message": "howdy"})
+					if err != nil {
+						panic(err)
+					}
+					result := out["hello"]
+					return containerResult{Result: result}
 				}}}})
+
 }
